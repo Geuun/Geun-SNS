a079da690a22057c6a6efdb72dc3f00473c1d987
KimJaeGeun <jgk9612@gmail.com>
2022-12-21 04:14:34 +0900

    fix: Dockerfile & SwaggerConfig

--- src/main/java/com/dev/geunsns/global/config/SwaggerConfig.java
+++ src/main/java/com/dev/geunsns/global/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.dev.geunsns.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -12,9 +14,18 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Geun-SNS Rest API Documentation")
+                .description("Geun-SNS")
+                .version("v1")
                 .build();
     }
 }
