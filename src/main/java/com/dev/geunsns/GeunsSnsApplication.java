45701b0c48d1382ce977d7e5609e888160d3f1c1
KimJaeGeun <jgk9612@gmail.com>
2022-12-20 11:47:28 +0900

    first commit

---
+++ src/main/java/com/dev/geunsns/GeunsSnsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.geunsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeunsSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeunsSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/geunsns/GeunsSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.geunsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeunsSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
