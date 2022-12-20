36ab04f18ba5ddddea33446197c16643b220bac3
KimJaeGeun <jgk9612@gmail.com>
2022-12-20 13:27:36 +0900

    feat: Add RootController for hello

---
+++ src/main/java/com/dev/geunsns/global/controller/RootController.java
@@ -0,0 +1,18 @@
+package com.dev.geunsns.global.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("")
+public class RootController {
+
+    @GetMapping("/api/v1/hello")
+    public String hello() {
+        return "hello";
+    }
+}
