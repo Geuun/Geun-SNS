73334e7969d0c8c8840b8328fc651401c3529f26
KimJaeGeun <jgk9612@gmail.com>
2022-12-21 17:32:33 +0900

    chore: Add dependencies
    아래 의존성을 추가해서 회원가입 및 로그인 기능 구현
    1. SpringSecurity
    2. JwtTokenFilter
    3. Encrypter encoder

---
+++ src/main/java/com/dev/geunsns/global/config/EncrypterConfig.java
@@ -0,0 +1,14 @@
+package com.dev.geunsns.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class EncrypterConfig {
+
+    @Bean // Pwd 암호화
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+}
