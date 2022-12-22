c86c665cac66b27aaf70551bd9291e9ceee723f9
KimJaeGeun <jgk9612@gmail.com>
2022-12-22 16:12:55 +0900

    refactor: Update for more careful exception handling

--- src/main/java/com/dev/geunsns/global/data/response/ExceptionManager.java
+++ src/main/java/com/dev/geunsns/global/data/response/ExceptionManager.java
@@ -1,27 +1,33 @@
-package com.dev.geunsns.apps.user.exception;
+package com.dev.geunsns.global.data.response;
 
+import com.dev.geunsns.apps.user.exception.UserAppException;
 import com.dev.geunsns.global.data.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
-public class UserExceptionManager {
+public class ExceptionManager {
 
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> runtimeException(RuntimeException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getCause(), e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Response.error(e.getMessage()));
+                .body(Response.error("SERVER_ERROR", errorResponse));
     }
 
+    //UserApp
     @ExceptionHandler(UserAppException.class)
     public ResponseEntity<?> userAppException(UserAppException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getUserErrorCode(), e.getUserErrorMessage());
+        log.info(String.valueOf(errorResponse));
         return ResponseEntity
-                .status(e.getErrorCode().getStatus()) // e 에서 상태코드 반환
-                .body(e.getErrorCode().getMessage()); // e 에서 메세지 반환
+                .status(e.getUserErrorCode().getHttpStatus()) // e 에서 상태코드 반환
+                .body(Response.error("ERROR", errorResponse)); // e 에서 메세지 반환
     }
-
 }
--- src/main/java/com/dev/geunsns/global/data/response/Response.java
+++ src/main/java/com/dev/geunsns/global/data/response/Response.java
@@ -7,17 +7,17 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Response <T> {
+public class Response<T> {
 
 
     private String resultCode;
     private T result;
 
-    public static Response<Void> error(String resultCode) {
-        return new Response(resultCode, null);
+    public static <T> Response<T> error(String resultCode, T result) {
+        return new Response(resultCode, result);
     }
 
-    public static  <T> Response<T> success(T result) {
-        return new Response("SUCCESS", result);
+    public static <T> Response<T> success(String resultCode, T result) {
+        return new Response(resultCode, result);
     }
 }
