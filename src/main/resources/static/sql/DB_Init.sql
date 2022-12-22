cca7bb1cd4642e7674c3067f7a8056453ac123b1
KimJaeGeun <jgk9612@gmail.com>
2022-12-22 09:22:24 +0900

    test: Test CICD

---
+++ src/main/resources/static/sql/DB_Init.sql
@@ -0,0 +1,9 @@
+# 개발용 DB 생성
+DROP DATABASE IF EXISTS geun_sns__dev;
+CREATE DATABASE geun_sns__dev;
+USE geun_sns__dev;
+
+# 테스트용 DB 생성
+DROP DATABASE IF EXISTS geun_sns__test;
+CREATE DATABASE geun_sns__test;
+USE geun_sns__test;
