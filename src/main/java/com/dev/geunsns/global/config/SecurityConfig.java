e7b3e43a01c57f0453a4b13f4ef59063ac1c4c35
KimJaeGeun <jgk9612@gmail.com>
2022-12-23 09:55:00 +0900

    fix: Fix SecurityConfig
    to get error fix

--- src/main/java/com/dev/geunsns/global/config/SecurityConfig.java
+++ src/main/java/com/dev/geunsns/global/config/SecurityConfig.java
@@ -1,48 +1,31 @@
 package com.dev.geunsns.global.config;
 
-import com.dev.geunsns.apps.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
-    private final UserService userService;
-
-    @Value("${jwt.token.secret}")
-    private String secretKey;
-
-    private final String[] SWAGGER = {
-            "/v3/api-docs",
-            "/swagger-resources/**",
-            "/swagger-ui/**",
-            "/webjars/**",
-            "/swagger/**"
-    };
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .httpBasic().disable()
-                .csrf().disable()
-                .cors().and()
-                .authorizeRequests()
-                .antMatchers(SWAGGER).permitAll()
-                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
-                .antMatchers("/api/**").authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //jwt
-                .and()
-                .build();
+            .httpBasic().disable()
+            .csrf().disable()
+            .cors().and()
+            .authorizeRequests()
+            .antMatchers("/api/**").permitAll()
+            .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //jwt
+            .and()
+            .build();
     }
 }
