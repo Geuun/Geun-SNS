581c45a0d485b67b8b2ce3966313b68691a38c4f
KimJaeGeun <jgk9612@gmail.com>
2022-12-25 05:39:07 +0900

    chore: Update application.yml

---
+++ src/main/java/com/dev/geunsns/apps/model/UserRole.java
@@ -0,0 +1,26 @@
+package com.dev.geunsns.apps.user.data.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+public enum UserRole {
+	/**
+	 * Spring Security Role Naming Rule : ROLE_AUTHORITY
+	 */
+
+	ROLE_SUPERADMIN("Super_Admin"),
+	ROLE_ADMIN("Admin"),
+	ROLE_USER("User"),
+	ROLE_ANONYMOUS("Anonymous"),
+	;
+
+	private final String role;
+
+	UserRole(String role) {
+		this.role = role;
+	}
+
+	public String getRole() {
+		return role;
+	}
+}
