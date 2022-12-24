b756098ebb11329f29d0115cac37d52fae3e6916
KimJaeGeun <jgk9612@gmail.com>
2022-12-25 05:45:07 +0900

    feat: Add Spring JPA Auditing to check the change log of the data

---
+++ src/main/java/com/dev/geunsns/global/config/AuditorAwareImpl.java
@@ -0,0 +1,27 @@
+package com.dev.geunsns.global.config;
+
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		Authentication authentication = SecurityContextHolder.getContext()
+															 .getAuthentication();
+
+		String userName = "";
+
+		if (authentication != null) {
+			/**
+			 * 로그인한 userName을 조회
+			 * -> 등록자와 수정자로 지정
+			 */
+			userName = authentication.getName();
+		}
+
+		return Optional.of(userName);
+	}
+}
---
+++ src/main/java/com/dev/geunsns/global/data/jpaauditing/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.dev.geunsns.global.data.jpaauditing.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity extends BaseTimeEntity{
+	/**
+	 * 다른 곳에서 인스턴스 생성하지 못하게 추상클래스로 선언
+	 */
+	@CreatedBy
+	@Column(updatable = false)
+	private String createdBy;
+
+	@LastModifiedBy
+	@Column
+	private String lastModifiedBy;
+}
