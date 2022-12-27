189e554cf238319b74ef551efa06ce4355a778f4
KimJaeGeun <jgk9612@gmail.com>
2022-12-27 16:45:31 +0900

    feat: Add Global Error Code

--- src/main/java/com/dev/geunsns/global/exception/GlobalErrorCode.java
+++ src/main/java/com/dev/geunsns/global/exception/GlobalErrorCode.java
@@ -1,4 +1,21 @@
 package com.dev.geunsns.global.exception;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public enum GlobalErrorCode {
+
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "You do not have access permission.");
+
+    private HttpStatus httpStatus;
+    private String errorMessage;
+
+    GlobalErrorCode(HttpStatus httpStatus, String errorMessage) {
+        this.httpStatus = httpStatus;
+        this.errorMessage = errorMessage;
+    }
 }
---
+++ src/main/java/com/dev/geunsns/global/exception/GlobalException.java
@@ -0,0 +1,26 @@
+package com.dev.geunsns.global.exception;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GlobalException extends RuntimeException{
+
+    private GlobalErrorCode ErrorCode;
+    private String ErrorMessage;
+
+    // 메세지 입력안했을 때 enum메세지 반환
+    public GlobalException(GlobalErrorCode ErrorCode) {
+        this.ErrorCode = ErrorCode;
+        this.ErrorMessage = ErrorCode.getErrorMessage();
+    }
+
+    // 메세지 직접 입력했을 때 해당 메세지 반환
+    public GlobalException(GlobalErrorCode ErrorCode, String ErrorMessage) {
+        this.ErrorCode = ErrorCode;
+        this.ErrorMessage = ErrorMessage;
+    }
+}
