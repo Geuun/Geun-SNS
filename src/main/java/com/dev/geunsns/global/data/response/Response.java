b24ae36b5303ab93799d316ff4cf2ada6fc9901b
KimJaeGeun <jgk9612@gmail.com>
2022-12-27 16:45:00 +0900

    refactor: Update exception codes

--- src/main/java/com/dev/geunsns/global/data/response/Response.java
+++ src/main/java/com/dev/geunsns/global/data/response/Response.java
@@ -1,18 +1,21 @@
 package com.dev.geunsns.global.data.response;
 
 import com.dev.geunsns.apps.user.data.dto.login.UserLoginResponse;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Response<T> {
 
 
 	private String resultCode;
 	private T result;
+
+	@Builder
+	public Response(String resultCode, T result) {
+		this.resultCode = resultCode;
+		this.result = result;
+	}
 
 	public static <T> Response<T> error(String resultCode, T result) {
 		return new Response(resultCode, result);
@@ -24,9 +27,5 @@ public class Response<T> {
 
 	public static <T> Response<T> success(T result) {
 		return new Response("SUCCESS", result);
-	}
-
-	public static Response<Void> success() {
-		return new Response("SUCCESS", null);
 	}
 }
