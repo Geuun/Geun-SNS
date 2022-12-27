be4e80301f2887d278411730db4f7145ff150aed
KimJaeGeun <jgk9612@gmail.com>
2022-12-27 16:40:53 +0900

    fix: Update path permission for setting security

--- src/main/java/com/dev/geunsns/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/dev/geunsns/global/config/swagger/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.dev.geunsns.global.config.swagger;
 
 import java.util.Arrays;
 import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -20,49 +21,49 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SwaggerConfig {
 
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.OAS_30)
-			.apiInfo(this.apiInfo())
-			.securityContexts(Arrays.asList(securityContext()))
-			.securitySchemes(Arrays.asList(apiKey()))
-			.select()
-			.apis(RequestHandlerSelectors.any())
-			.paths(PathSelectors.ant("/api/**"))
-			.build();
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(this.apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
 
-	@Bean
-	public InternalResourceViewResolver defaultViewResolver() {
-		return new InternalResourceViewResolver();
-	}
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
 
-	public ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-			.title("Geun-SNS Rest API Documentation")
-			.description("Geun-SNS")
-			.version("v1")
-			.build();
-	}
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Geun-SNS Rest API Documentation")
+                .description("Geun-SNS")
+                .version("v1")
+                .build();
+    }
 
-	private ApiKey apiKey() { // button 클릭 시 입력하는 값 설정
-		return new ApiKey("Authorization",
-						  "Authorization",
-						  "header");
-	}
+    private ApiKey apiKey() { // button 클릭 시 입력하는 값 설정
+        return new ApiKey("Authorization",
+                "Authorization",
+                "header");
+    }
 
-	private SecurityContext securityContext() { // 인증 방식 설정
-		return SecurityContext.builder()
-							  .securityReferences(defaultAuth())
-							  .build();
-	}
+    private SecurityContext securityContext() { // 인증 방식 설정
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
 
-	private List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global",
-																	   "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return Arrays.asList(new SecurityReference("Authorization",
-												   authorizationScopes));
-	}
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global",
+                "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization",
+                authorizationScopes));
+    }
 }
