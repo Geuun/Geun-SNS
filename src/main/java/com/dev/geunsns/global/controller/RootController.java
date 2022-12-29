ada6dfd08c2381d53dbdbfe751135d3557261a9d
KimJaeGeun <jgk9612@gmail.com>
2022-12-29 10:07:20 +0900

    fix: Add annotation

--- src/main/java/com/dev/geunsns/global/controller/RootController.java
+++ src/main/java/com/dev/geunsns/global/controller/RootController.java
@@ -2,6 +2,7 @@ package com.dev.geunsns.global.controller;
 
 
 import com.dev.geunsns.global.service.AlgorithmService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1")
 public class RootController {
