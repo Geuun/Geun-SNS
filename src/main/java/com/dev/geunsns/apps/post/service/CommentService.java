3a62b52d672063b5eea3a21f4b4f2554b71d5d25
KimJaeGeun <jgk9612@gmail.com>
2023-01-01 05:19:41 +0900

    fix: Fix & Refactor Post App's Comment API

--- src/main/java/com/dev/geunsns/apps/post/service/CommentService.java
+++ src/main/java/com/dev/geunsns/apps/post/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.dev.geunsns.apps.post.service;
 
+import com.dev.geunsns.apps.model.UserRole;
 import com.dev.geunsns.apps.post.data.dto.comment.CommentDto;
 import com.dev.geunsns.apps.post.data.dto.comment.CommentRequest;
 import com.dev.geunsns.apps.post.data.dto.comment.CommentResponse;
@@ -13,11 +14,13 @@ import com.dev.geunsns.apps.user.data.entity.UserEntity;
 import com.dev.geunsns.apps.user.repository.UserRepository;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Collection;
 import java.util.Objects;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -99,8 +102,8 @@ public class CommentService {
     }
 
     @Transactional
-    public Boolean deleteComment(Long id, String userName) {
-        CommentEntity entity = commentRepository.findById(id)
+    public CommentDto deleteComment(Long id, String userName, Collection<? extends GrantedAuthority> authorities) {
+        CommentEntity commentEntity = commentRepository.findById(id)
                 .orElseThrow(() -> new PostAppException(PostAppErrorCode.POST_NOT_FOUND,
                         String.format("Comment id #%d was not found", id)));
 
@@ -110,13 +113,20 @@ public class CommentService {
 
         Long userId = userEntity.getId();
 
-        if (!Objects.equals(entity.getUser()
-                .getId(), userId)) {
+        if (!Objects.equals(commentEntity.getUser().getId(), userId) && !authorities.stream().findFirst().get().getAuthority().equals(UserRole.ROLE_ADMIN.toString())) {
             throw new PostAppException(PostAppErrorCode.INVALID_PERMISSION, String.format("User #%s  do not have access to Comment %d.", userId, id));
         }
 
-        commentRepository.delete(entity);
+        try {
+            commentRepository.delete(commentEntity);
+        } catch (Exception e) {
+            throw new PostAppException(PostAppErrorCode.DATABASE_ERROR);
+        }
 
-        return true;
+        CommentDto deletedComment = CommentDto.builder()
+                .id(id)
+                .build();
+
+        return deletedComment;
     }
 }
